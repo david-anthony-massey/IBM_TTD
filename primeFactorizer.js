1ba88fcb579c9cb90f9c135f2a2a6b825e3b6362
David M <d.anthony.massey@gmail.com>
2021-03-20 23:25:06 -0500

    Pass test number 1

---
+++ primeFactorizer.js
@@ -0,0 +1,3 @@
+const primeFactorizer = number => [];
+
+module.exports = { primeFactorizer };
---
+++ test/primeFactorizer.test.js
@@ -0,0 +1,17 @@
+const { primeFactorizer } = require('../primeFactorizer');
+const expect = require('chai').expect;
+
+describe.only(`isPrime test suite`, () => {
+  describe(`isPrime canary spec`, () => {
+    it('shows the infrastructure works', () => {
+      expect(true).to.be.true;
+    });
+  });
+  describe(`primeFactorizer correctly outputs all prime factors`, () => {
+    it('for 1 return []', () => {
+      expect(primeFactorizer(1)).to.deep.equal([]);
+    });
+    it('for 2 return [2]');
+    it('for 4 return [2,2]');
+  });
+});
